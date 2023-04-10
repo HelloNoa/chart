d22ef4f058f1e25ed838e19051b0002b905b1ffe
노아 <ohyoungwoo824@gmail.com>
2023-04-10 23:37:41 +0900

    fix: redis cluster forRootAsync

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -43,7 +43,11 @@ import { order_book_differenceModule } from '../typeorm/order_book_difference/or
       isGlobal: true,
       cache: true,
     }),
-    RedisClusterModule.register(redisClusterOptions()),
+    RedisClusterModule.forRootAsync({
+      useFactory: () => {
+        return redisClusterOptions();
+      },
+    }),
     RedisPubSubModule,
     SocketModule,
     ChartModule,
