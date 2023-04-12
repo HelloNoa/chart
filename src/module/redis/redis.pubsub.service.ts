2c3c1790fb6da23d2ac273fa0a49a0b99f6cbb65
노아 <ohyoungwoo824@gmail.com>
2023-04-13 01:18:59 +0900

    feat: order book

--- src/module/redis/redis.pubsub.service.ts
+++ src/module/redis/redis.pubsub.service.ts
@@ -3,13 +3,16 @@ import { channel, OrderMatching } from '../../dto/redis.dto.js';
 import * as IORedis from 'ioredis';
 import { ChartGateway } from '../socket/gateway/chart.gateway.js';
 import { RedisClusterService } from 'nestjs-redis-cluster';
+import { OrderBookService } from '../orderBook/orderBook.service.js';
 
 @Injectable()
 export class RedisPubSubService {
   private client: IORedis.Cluster;
+
   constructor(
     private readonly redisService: RedisClusterService,
     private readonly chartSocketService: ChartGateway,
+    private readonly orderBookService: OrderBookService,
   ) {
     this.client = this.redisService.getCluster('REDIS_SERVICE');
     // console.log(this.client);
@@ -25,10 +28,27 @@ export class RedisPubSubService {
         console.log('-----------------');
         console.log(ch);
         console.log(msg);
+        const req = {
+          symbol: msg.Symbol,
+          type: -1,
+          quantity: msg.Quantity,
+          unitPrice: msg.UnitPrice,
+          orderType: msg.OrderType,
+        };
         switch (ch) {
           //tradeEvent for chartdraw 차트 구독
           case 'OrderMatchingChannel':
             this.OrderMatching(msg);
+            req.type = 0;
+            this.OrderBook(req);
+            break;
+          case 'OrderPlacementChannel':
+            req.type = 1;
+            this.OrderBook(req);
+            break;
+          case 'OrderCancellationChannel':
+            req.type = 2;
+            this.OrderBook(req);
             break;
           default:
             break;
@@ -36,6 +56,31 @@ export class RedisPubSubService {
         console.log('-----------------');
       });
     });
+  }
+
+  OrderBook(req: {
+    symbol: string;
+    type: number;
+    quantity: number;
+    unitPrice: number;
+    orderType: string;
+  }) {
+    switch (req.type) {
+      case 0:
+        //OrderMatchingChannel
+        this.orderBookService.updateOrderBook(req);
+        break;
+      case 1:
+        //OrderPlacementChannel
+        this.orderBookService.updateOrderBook(req);
+        break;
+      case 2:
+        //OrderCancellationChannel
+        this.orderBookService.updateOrderBook(req);
+        break;
+      default:
+        break;
+    }
   }
 
   //tradeEvent for chartdraw 차트 구독
