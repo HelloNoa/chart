a64a46aa678c4bfa16faef273ac3607ee5a47ce2
노아 <ohyoungwoo824@gmail.com>
2023-04-14 01:40:55 +0900

    feat: remove DepthGateway

--- src/module/socket/socket.module.ts
+++ src/module/socket/socket.module.ts
@@ -1,11 +1,10 @@
-import { DepthGateway } from './gateway/depth.gateway.js';
 import { Module } from '@nestjs/common';
 import { ChartGateway } from './gateway/chart.gateway.js';
 
 @Module({
   imports: [],
   // controllers: [SocketController],
-  providers: [DepthGateway, ChartGateway],
-  exports: [DepthGateway, ChartGateway],
+  providers: [ChartGateway],
+  exports: [ChartGateway],
 })
 export class SocketModule {}
