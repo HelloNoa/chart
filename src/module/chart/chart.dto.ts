eaea1636276125c7bef839d9df1a4c7ee37d0979
노아 <ohyoungwoo824@gmail.com>
2023-04-15 04:01:54 +0900

    fix: chart rest dto

--- src/module/chart/chart.dto.ts
+++ src/module/chart/chart.dto.ts
@@ -1,6 +1,8 @@
+import { INTERVAL } from '../typeorm/order_interval/order_interval.entity.js';
+
 export interface ChartReqDto {
   order_symbol_id: string;
-  interval: string;
+  interval: keyof INTERVAL;
   length: number;
   created_at: string;
 }
