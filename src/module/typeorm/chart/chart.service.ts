9f69557e9a67e3c8d87587c65d5d5b50c12005d4
노아 <ohyoungwoo824@gmail.com>
2023-04-24 16:34:05 +0900

    fix: orderBook

--- src/module/typeorm/chart/chart.service.ts
+++ src/module/typeorm/chart/chart.service.ts
@@ -27,12 +27,19 @@ export class chartService {
       INTERVAL.ONE_DAY,
     );
     const queryBuilder = this.chartRepository.createQueryBuilder('chart');
-    queryBuilder
-      .select('chart.created_at', 't')
-      .addSelect('chart.close_price', 'c')
-      .addSelect('chart.volume', 'v')
-      .where('chart.order_interval_id in (:...intervarId)', { intervarId })
-      .orderBy('chart.created_at', 'ASC');
+    if (intervarId.length === 0) {
+      console.log('intervarId length is 0');
+      return null;
+    } else {
+      queryBuilder
+        .select('chart.created_at', 't')
+        .addSelect('chart.close_price', 'c')
+        .addSelect('chart.volume', 'v')
+        .where('chart.order_interval_id in (:...intervarId)', {
+          intervarId,
+        })
+        .orderBy('chart.created_at', 'ASC');
+    }
     const data = await queryBuilder.getRawMany();
     if (data.length === 0) {
       console.error('chart data not found');
@@ -53,18 +60,26 @@ export class chartService {
         interval,
       );
       const queryBuilder = this.chartRepository.createQueryBuilder('chart');
-      queryBuilder
-        .select('chart.open_price', 'o')
-        .addSelect('chart.low_price', 'l')
-        .addSelect('chart.high_price', 'h')
-        .addSelect('chart.close_price', 'c')
-        .addSelect('chart.trading_value', 'v')
-        .addSelect('chart.created_at', 't')
-        .where('chart.order_symbol_id = :id', { id })
-        .andWhere('chart.order_interval_id in (:...intervarId)', { intervarId })
-        .andWhere('chart.created_at < :created_at', { created_at })
-        .orderBy('chart.created_at', 'ASC')
-        .take(length);
+
+      if (intervarId.length === 0) {
+        console.log('intervarId length is 0');
+        return null;
+      } else {
+        queryBuilder
+          .select('chart.open_price', 'o')
+          .addSelect('chart.low_price', 'l')
+          .addSelect('chart.high_price', 'h')
+          .addSelect('chart.close_price', 'c')
+          .addSelect('chart.volume', 'v')
+          .addSelect('chart.created_at', 't')
+          .where('chart.order_symbol_id = :id', { id })
+          .andWhere('chart.order_interval_id in (:...intervarId)', {
+            intervarId,
+          })
+          .andWhere('chart.created_at < :created_at', { created_at })
+          .orderBy('chart.created_at', 'ASC')
+          .take(length);
+      }
       const data = await queryBuilder.getRawMany();
       if (data.length === 0) {
         console.error('chart data not found');
