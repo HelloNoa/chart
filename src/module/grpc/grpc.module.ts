f18ceccabf4fb0e4637be80e2bf708477ed4f640
노아 <ohyoungwoo824@gmail.com>
2023-04-29 18:59:32 +0900

    feat: import SocketModule in grpc module

--- src/module/grpc/grpc.module.ts
+++ src/module/grpc/grpc.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { GrpcController } from './grpc.controller.js';
+import { SocketModule } from '../socket/socket.module.js';
 
 @Module({
-  imports: [],
+  imports: [forwardRef(() => SocketModule)],
   controllers: [GrpcController],
 })
 export class GrpcModule {}
