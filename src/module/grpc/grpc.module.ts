006524ab52a71ef459ac5a2fb20a285f432f2f97
노아 <ohyoungwoo824@gmail.com>
2023-05-02 03:11:51 +0900

    fix: grpc server logic

--- src/module/grpc/grpc.module.ts
+++ src/module/grpc/grpc.module.ts
@@ -1,9 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { GrpcController } from './grpc.controller.js';
 import { SocketModule } from '../socket/socket.module.js';
+import { OrderBookModule } from '../orderBook/orderBook.module.js';
 
 @Module({
-  imports: [forwardRef(() => SocketModule)],
+  imports: [OrderBookModule, forwardRef(() => SocketModule)],
   controllers: [GrpcController],
 })
 export class GrpcModule {}
