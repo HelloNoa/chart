157e57f17b54366c50600c07bfd0ce539500b66d
노아 <ohyoungwoo824@gmail.com>
2023-05-09 02:29:27 +0900

    feat: import ticker in grpc module

--- src/module/grpc/grpc.module.ts
+++ src/module/grpc/grpc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GrpcController } from './grpc.controller.js';
 import { SocketModule } from '../socket/socket.module.js';
 import { OrderBookModule } from '../inMemory/orderBook/orderBook.module.js';
+import { TickerModule } from '../inMemory/ticker/ticker.module.js';
 
 @Module({
-  imports: [OrderBookModule, SocketModule],
+  imports: [OrderBookModule, TickerModule, SocketModule],
   controllers: [GrpcController],
 })
 export class GrpcModule {}
