1520f0c4ff1d2bc4562e64efa6f71a3ef09b07b3
노아 <ohyoungwoo824@gmail.com>
2023-05-08 18:27:09 +0900

    test: socket test3

--- src/module/grpc/grpc.module.ts
+++ src/module/grpc/grpc.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { GrpcController } from './grpc.controller.js';
 import { SocketModule } from '../socket/socket.module.js';
 import { OrderBookModule } from '../orderBook/orderBook.module.js';
