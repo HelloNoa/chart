68a99870140ca1c8fb6121cfedd03e059a337ba8
노아 <ohyoungwoo824@gmail.com>
2023-05-09 03:41:40 +0900

    fix: add order_intervalModule in ticker module

--- src/module/inMemory/ticker/ticker.module.ts
+++ src/module/inMemory/ticker/ticker.module.ts
@@ -3,9 +3,15 @@ import { order_symbolModule } from '../../typeorm/order_symbol/order_symbol.modu
 import { SocketModule } from '../../socket/socket.module.js';
 import { chartModule } from '../../typeorm/chart/chart.module.js';
 import { TickerService } from './ticker.service.js';
+import { order_intervalModule } from '../../typeorm/order_interval/order_interval.module.js';
 
 @Module({
-  imports: [SocketModule, chartModule, order_symbolModule],
+  imports: [
+    SocketModule,
+    chartModule,
+    order_symbolModule,
+    order_intervalModule,
+  ],
   providers: [TickerService],
   exports: [TickerService],
 })
