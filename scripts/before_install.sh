86dd63f6b09004bb5ec666d8aef9f9064de62a73
huijikim00 <128798174+huijikim00@users.noreply.github.com>
2023-05-17 15:44:46 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/ec2-user/s3
 mkdir -p /home/ec2-user/finexblock/src/config/env
 
-aws s3 cp s3://finexblock-env2/coin-market/dev/chart-server/.dev.env /home/ec2-user/finexblock/src/config/env/.dev.env
+aws s3 cp s3://finexblock-env2-dev/coin-market/dev/chart-server/.dev.env /home/ec2-user/finexblock/src/config/env/.dev.env
 
 
 # cat /home/ec2-user/finexblock/src/config/env/.prod.env
