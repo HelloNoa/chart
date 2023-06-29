544208bb5e3ab38e3fa45a1be65d11dd92283cba
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-06-29 22:28:22 +0900

    fix: add allow ips

--- src/decorators/ip-guard.service.ts
+++ src/decorators/ip-guard.service.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
-const allowIps = ['121.134.226.223', '::1'];
+const allowIps = ['121.134.226.223', '::1', '61.78.96.41', '218.145.191.242'];
 @Injectable()
 export class IpGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
