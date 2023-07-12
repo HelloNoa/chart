08d48d459494fe2411303ed765cb3400a23534f9
노아 <ohyoungwoo824@gmail.com>
2023-07-12 16:48:15 +0900

    feat: 임시

--- src/module/redis/redis.pubsub.module.ts
+++ src/module/redis/redis.pubsub.module.ts
@@ -1,12 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { RedisPubSubService } from './redis.pubsub.service.js';
 import { SocketModule } from '../socket/socket.module.js';
+import { RedisPubSubController } from './redis.pubsub.controller.js';
 
 @Module({
   imports: [forwardRef(() => SocketModule)],
-  controllers: [
-    // RedisPubSubController
-  ],
+  controllers: [RedisPubSubController],
   providers: [RedisPubSubService],
   exports: [RedisPubSubService],
 })
